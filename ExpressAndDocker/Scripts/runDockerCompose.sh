79e670fea5b44f5cda8f8dd6941f49ff97440842
Sandro <sandro.spengler@web.de>
2021-10-23 13:54:09 +0200

    SSL

--- ExpressAndDocker/Scripts/runDockerCompose.sh
+++ ExpressAndDocker/Scripts/runDockerCompose.sh
@@ -6,7 +6,7 @@ docker-compose down --remove-orphans -v
 
 docker-compose \
     -f docker-compose.yaml \
-    -f docker-compose.prod.yaml \
+    -f docker-compose.dev.yaml \
     up -d \
     --build \
     # runs only one container
